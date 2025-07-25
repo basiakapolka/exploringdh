fa86a565c1cca7d7f59d5739af72c98976506e50
basiakapolka <basia.kapolka@northwestern.edu>
2025-07-25 16:06:56 -0500

    Add files via upload

---
+++ assets/js/java.js
@@ -0,0 +1,12 @@
+
+
+function toggleTheme() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+function toggleFont() {
+  var text = document.body;
+  text.classList.toggle("fancy-font");
+
+}
